d0bd831047229c109f8e34d535c2bcbffa3fb765
AkhnasFurqan-Dev <furqanabid782@gmail.com>
2026-08-28 17:59:07 +0500

    added course model and registered to courseadmin, admin

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "teacher", "duration", "schedule", "created_at")
+    list_filter = ("teacher",)
+    search_fields = ("title", "description")
